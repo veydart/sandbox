c3e4e3295d9989c3b6cca18dd77a2e290c75591f
aylaylay <fortblox@gmail.com>
2021-04-16 20:55:58 +0100

    Add directional gravity entity

---
+++ code/entities/DirectionalGravity.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+
+[ClassLibrary( "directional_gravity", Title = "Directional Gravity", Spawnable = true )]
+public partial class DirectionalGravity : Prop, IPhysicsUpdate
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/arrow.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+	}
+
+	protected override void OnDestroy()
+	{
+		base.OnDestroy();
+
+		if ( IsServer )
+		{
+			// We probably want a better way to restore previous gravity
+			PhysicsWorld.Gravity = Vector3.Down * 800.0f;
+		}
+	}
+
+	public void OnPostPhysicsStep( float dt )
+	{
+		if ( !IsServer )
+			return;
+
+		if ( !PhysicsBody.IsValid() )
+			return;
+
+		PhysicsWorld.Gravity = PhysicsBody.Rot.Down * 800.0f;
+	}
+}
