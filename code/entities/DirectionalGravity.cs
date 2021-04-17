3ed1f5a5a69de88b95d690c7d4c68f8680ae8a91
aylaylay <fortblox@gmail.com>
2021-04-17 20:52:30 +0100

    Only allow one directional gravity to be spawned at a time

--- code/entities/DirectionalGravity.cs
+++ code/entities/DirectionalGravity.cs
@@ -1,4 +1,5 @@
 ﻿using Sandbox;
+using System.Linq;
 
 [ClassLibrary( "directional_gravity", Title = "Directional Gravity", Spawnable = true )]
 public partial class DirectionalGravity : Prop, IPhysicsUpdate
@@ -6,6 +7,13 @@ public partial class DirectionalGravity : Prop, IPhysicsUpdate
 	public override void Spawn()
 	{
 		base.Spawn();
+
+		// Only allow one of these to be spawned at a time
+		foreach ( var ent in All.OfType<DirectionalGravity>()
+			.Where( x => x.IsValid() && x != this ))
+		{
+			ent.Delete();
+		}
 
 		SetModel( "models/arrow.vmdl" );
 		SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
@@ -27,9 +35,9 @@ public partial class DirectionalGravity : Prop, IPhysicsUpdate
 		if ( !IsServer )
 			return;
 
-		if ( !PhysicsBody.IsValid() )
+		if ( !this.IsValid() )
 			return;
 
-		PhysicsWorld.Gravity = PhysicsBody.Rot.Down * 800.0f;
+		PhysicsWorld.Gravity = WorldRot.Down * 800.0f;
 	}
 }
