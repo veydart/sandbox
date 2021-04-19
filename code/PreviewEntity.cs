b079d9a1c1d517993295c5e26953186d9a88d026
aylaylay <fortblox@gmail.com>
2021-04-19 21:58:17 +0100

    Add lamp tool

--- code/PreviewEntity.cs
+++ code/PreviewEntity.cs
@@ -1,11 +1,12 @@
-﻿using System;
-
+﻿
 namespace Sandbox.Tools
 {
 	public class PreviewEntity : ModelEntity
 	{
 		public bool RelativeToNormal { get; set; } = true;
+		public bool OffsetBounds { get; set; } = false;
 		public Rotation RotationOffset { get; set; } = Rotation.Identity;
+		public Vector3 PositionOffset { get; set; } = Vector3.Zero;
 
 		internal void UpdateFromTrace( TraceResult tr )
 		{
@@ -15,18 +16,22 @@ namespace Sandbox.Tools
 				return;
 			}
 
-			WorldPos = tr.EndPos;
-
 			if ( RelativeToNormal )
 			{
 				WorldRot = Rotation.LookAt( tr.Normal, tr.Direction ) * RotationOffset;
+				WorldPos = tr.EndPos + WorldRot * PositionOffset;
 			}
 			else
 			{
 				WorldRot = Rotation.Identity * RotationOffset;
+				WorldPos = tr.EndPos + PositionOffset;
 			}
 
-			
+			if ( OffsetBounds )
+			{
+				WorldPos += tr.Normal * CollisionBounds.Size * 0.5f;
+			}
+
 			RenderAlpha = 0.5f;
 		}
 
---
+++ code/tools/Lamp.cs
@@ -0,0 +1,70 @@
+﻿namespace Sandbox.Tools
+{
+	[ClassLibrary( "tool_lamp", Title = "Lamp", Group = "Lighting" )]
+	public partial class LampTool : BaseTool
+	{
+		PreviewEntity previewModel;
+
+		private string Model { get; set; } = "models/torch/torch.vmdl";
+
+		public override void CreatePreviews()
+		{
+			if ( TryCreatePreview( ref previewModel, Model ) )
+			{
+				previewModel.RelativeToNormal = false;
+				previewModel.OffsetBounds = true;
+				previewModel.PositionOffset = -previewModel.CollisionBounds.Center;
+			}
+		}
+
+		public override void OnPlayerControlTick()
+		{
+			if ( !Host.IsServer )
+				return;
+
+			using ( Prediction.Off() )
+			{
+				var input = Owner.Input;
+
+				if ( !input.Pressed( InputButton.Attack1 ) )
+					return;
+
+				var startPos = Owner.EyePos;
+				var dir = Owner.EyeRot.Forward;
+
+				var tr = Trace.Ray( startPos, startPos + dir * MaxTraceDistance )
+					.Ignore( Owner )
+					.Run();
+
+				if ( !tr.Hit || !tr.Entity.IsValid() )
+					return;
+
+				if ( tr.Entity is SpotLightEntity )
+				{
+					// TODO: Set properties
+
+					return;
+				}
+
+				var light = new SpotLightEntity
+				{
+					Enabled = true,
+					DynamicShadows = true,
+					Range = 512,
+					Falloff = 1.0f,
+					LinearAttenuation = 0.0f,
+					QuadraticAttenuation = 1.0f,
+					InnerConeAngle = 25,
+					OuterConeAngle = 45,
+					Brightness = 10,
+					Color = Color.Random,
+					Rot = Rotation.Identity
+				};
+
+				light.SetModel( Model );
+				light.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+				light.Pos = tr.EndPos + -light.CollisionBounds.Center + tr.Normal * light.CollisionBounds.Size * 0.5f;
+			}
+		}
+	}
+}
