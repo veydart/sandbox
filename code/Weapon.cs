afb9722821b6494e1097d0e3b47bbf84a17b4382
aylaylay <fortblox@gmail.com>
2021-04-25 11:11:53 +0100

    Give weapons a pickup trigger

--- code/Weapon.cs
+++ code/Weapon.cs
@@ -2,6 +2,19 @@
 
 public partial class Weapon : BaseWeapon
 {
+	public PickupTrigger PickupTrigger { get; protected set; }
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "weapons/rust_pistol/rust_pistol.vmdl" );
+
+		PickupTrigger = new PickupTrigger();
+		PickupTrigger.Parent = this;
+		PickupTrigger.WorldPos = WorldPos;
+	}
+
 	public override void CreateViewModel()
 	{
 		Host.AssertClient();
