6c72cc8bc28f4e26b21d48e9805c1d98f9db3b98
aylaylay <fortblox@gmail.com>
2021-04-27 05:20:41 +0100

    Admire flashlight with attack2

--- code/tools/Flashlight.cs
+++ code/tools/Flashlight.cs
@@ -86,6 +86,11 @@ partial class Flashlight : Weapon
 
 			timeSinceLightToggled = 0;
 		}
+
+		if ( input.Pressed( InputButton.Attack2 ) )
+		{
+			ViewModelEntity?.SetAnimParam( "admire", true );
+		}
 	}
 
 	private void Activate()
