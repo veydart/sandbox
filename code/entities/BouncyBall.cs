3be1ab1846c53b1aee778fd2a161b4fd557b6f1d
aylaylay <fortblox@gmail.com>
2021-04-28 11:33:37 +0100

    eat balls

--- code/entities/BouncyBall.cs
+++ code/entities/BouncyBall.cs
@@ -2,7 +2,7 @@
 using System;
 
 [Library( "ent_bouncyball", Title = "Bouncy Ball", Spawnable = true )]
-public partial class BouncyBallEntity : Prop
+public partial class BouncyBallEntity : Prop, IUse
 {
 	public float MaxSpeed { get; set; } = 1000.0f;
 	public float SpeedMul { get; set; } = 1.2f;
@@ -20,5 +20,22 @@ public partial class BouncyBallEntity : Prop
 		var speed = eventData.PreVelocity.Length;
 		var direction = Vector3.Reflect( eventData.PreVelocity.Normal, eventData.Normal.Normal ).Normal;
 		Velocity = direction * MathF.Min( speed * SpeedMul, MaxSpeed );
+	}
+
+	public bool IsUsable( Entity user )
+	{
+		return true;
+	}
+
+	public bool OnUse( Entity user )
+	{
+		if ( user is Player player )
+		{
+			player.Health += 10;
+
+			Delete();
+		}
+
+		return false;
 	}
 }
