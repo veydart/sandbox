3b0b8574206bcc5e53dc66c7151d8aad862ae873
Maxime Lebled <ml.maxofs2d@gmail.com>
2021-06-20 03:08:22 +0200

    Pistol firing should set b_attack to true

--- code/weapons/Pistol.cs
+++ code/weapons/Pistol.cs
@@ -26,6 +26,8 @@ partial class Pistol : Weapon
 	{
 		TimeSincePrimaryAttack = 0;
 		TimeSinceSecondaryAttack = 0;
+		
+		(Owner as AnimEntity)?.SetAnimBool( "b_attack", true );
 
 		ShootEffects();
 		PlaySound( "rust_pistol.shoot" );
