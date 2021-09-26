877b27b632c563bf15cea29def21d61a6c138842
aylaylay <fortblox@gmail.com>
2021-09-26 17:50:57 +0100

    fix not being able to drive car

--- code/Player.cs
+++ code/Player.cs
@@ -7,11 +7,11 @@ partial class SandboxPlayer : Player
 
 	private DamageInfo lastDamage;
 
-	[Net] public PawnController VehicleController { get; set; }
+	[Net] public PawnController VehicleController { get; set; } = null;
 	[Net] public PawnAnimator VehicleAnimator { get; set; }
-	[Net, Predicted] public ICamera VehicleCamera { get; set; }
-	[Net, Predicted] public Entity Vehicle { get; set; }
-	[Net, Predicted] public ICamera MainCamera { get; set; }
+	[Net, Predicted] public ICamera VehicleCamera { get; set; } = null;
+	[Net, Predicted] public Entity Vehicle { get; set; } = null;
+	[Net, Predicted] public ICamera MainCamera { get; set; } = null;
 
 	public ICamera LastCamera { get; set; }
 
