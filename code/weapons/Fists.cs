cfe75f053838e05b8da8f223a262614a5bdd0ed3
Jake Evans <jacob.ee92@gmail.com>
2022-05-04 19:23:16 -0600

    Set fists b_jump while not grounded due to pressing jump

--- code/weapons/Fists.cs
+++ code/weapons/Fists.cs
@@ -40,6 +40,7 @@ partial class Fists : Weapon
 	{
 	}
 
+	private bool jumped;
 	public override void SimulateAnimator( PawnAnimator anim )
 	{
 		anim.SetAnimParameter( "holdtype", 5 );
@@ -47,8 +48,12 @@ partial class Fists : Weapon
 
 		if ( Owner.IsValid() )
 		{
+			if ( Owner.GroundEntity.IsValid() )
+				jumped = Input.Pressed( InputButton.Jump );
+
 			ViewModelEntity?.SetAnimParameter( "b_grounded", Owner.GroundEntity.IsValid() );
 			ViewModelEntity?.SetAnimParameter( "aim_pitch", Owner.EyeRotation.Pitch() );
+			ViewModelEntity?.SetAnimParameter( "b_jump", jumped );
 		}
 	}
 
